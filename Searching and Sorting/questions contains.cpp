e1e385ff7266351f45da3ed4bdf4053fb4d7909d
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-04-07 17:45:01 +0530

    Update questions contains.cpp

--- Searching and Sorting/questions contains.cpp
+++ Searching and Sorting/questions contains.cpp
@@ -1,1 +1,35 @@
-
+Find first and last positions of an element in a sorted array	
+Find a Fixed Point (Value equal to index) in a given array	
+Search in a rotated sorted array	
+Square root of an integer	
+Maximum and minimum of an array using minimum number of comparisons	
+Optimum location of point to minimize total distance	
+Find the repeating and the missing	
+Find majority element	
+Searching in an array where adjacent differ by at most k
+Find a pair with a given difference	
+Find four elements that sum to a given value	
+Maximum sum such that no 2 elements are adjacent	
+Count triplet with sum smaller than a given value
+Merge 2 sorted arrays
+Product array Puzzle	
+Sort array according to count of set bits
+Minimum no. of swaps required to sort the array
+Bishu and Soldiers	
+Rasta and Kheshtak	
+Kth smallest number again	
+Find pivot element in a sorted array	
+K-th Element of Two Sorted Arrays	
+Aggressive cows	
+Book Allocation Problem	
+EKOSPOJ:	
+Job Scheduling Algo
+Missing Number in AP	
+Smallest number with atleast n trailing zeroes in factorial
+Painters Partition Problem:
+ROTI-Prata SPOJ	
+DoubleHelix SPOJ	
+Subset Sums	
+Find the inversion count	
+Implement Merge-sort in-place	
+Partitioning and Sorting Arrays with Many Repeated Entries
