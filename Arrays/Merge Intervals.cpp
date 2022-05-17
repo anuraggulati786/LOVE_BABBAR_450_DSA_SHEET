0e2cfad8f9f83a92e75e85437c1c02a6000d98f6
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-05-17 14:55:58 +0530

    Add files via upload

---
+++ Arrays/Merge Intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // sorted according to the first index of intervals...
+        // time complexity will become O(NLogN)... // n is the number of intervals in the interval array...
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> merged;
+        
+        for (auto interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.empty() || merged.back()[1] < interval[0]) {
+                merged.push_back(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.back()[1] = max(merged.back()[1], interval[1]);
+            }
+        }
+        return merged;
+    }
+};
