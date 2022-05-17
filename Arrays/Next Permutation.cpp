a961d045501f34162bf93d47ee5712454b397f0e
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-05-17 15:50:18 +0530

    Add files via upload

---
+++ Arrays/Next Permutation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    // time complexity O(N)...
+    // space is : O(1)...
+      void reverse(vector<int>& nums, int start) {
+        int i = start, j = nums.size() - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+     void swap(vector<int>& nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    
+    
+    void nextPermutation(vector<int>& nums) {
+    	 int i = nums.size() - 2;
+        while (i >= 0 && nums[i] >= nums[i+1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+        }
+}; 
