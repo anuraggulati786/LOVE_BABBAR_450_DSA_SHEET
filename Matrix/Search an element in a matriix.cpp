e907660cd18d89de128517ba0ef08c3957b3330f
Anurag_786 <76244073+anuraggulati786@users.noreply.github.com>
2022-05-27 13:50:34 +0530

    Add files via upload

---
+++ Matrix/Search an element in a matriix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+	
+	// *******************OPTAMIZED APPROACH **************
+	
+	// time O(R+C)................
+	// space O(1).................
+	
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        
+        int i = 0, j = matrix[0].size()-1;
+        
+        while(i<matrix.size() && j>=0) {
+            if(matrix[i][j] == target)return true;
+            else if(matrix[i][j] > target) {
+                j--;
+            }
+            else {i++;}
+        }
+        return false;
+    }
+};
